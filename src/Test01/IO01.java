3903b3aeb6dc45e4161a32b625ee46edd57de8dc
sorthorange <752893258@qq.com>
2019-06-06 13:43:55 +0800

    My Java Study

--- src/Test01/IO01.java
+++ src/Test01/IO01.java
@@ -1,4 +1,24 @@
 package Test01;
 
+import java.io.File;
+import java.io.IOException;
+
 public class IO01 {
+
+
+
+    public static void main(String[] args) throws IOException {
+        File src = new File("IO_Test.txt");
+        src.createNewFile();
+        if(null == src || !src.exists()) {
+            System.out.println("文件不存在");
+        }else {
+            if(src.isDirectory()) {
+                System.out.println("文件夹");
+            }else {
+                System.out.println("文件");
+            }
+        }
+    }
+
 }
