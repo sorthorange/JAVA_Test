bd156bc7e11841628cf7467877b7ed43f22102c7
sorthorange <752893258@qq.com>
2019-06-06 14:03:09 +0800

    My Java Study

--- src/Test01/Thread01.java
+++ src/Test01/Thread01.java
@@ -1,4 +1,30 @@
 package Test01;
 
+/**
+ *
+ *
+ */
 public class Thread01 {
+    public static void main(String[] args) {
+        God god = new God();
+        You you = new You();
+        new Thread(god).start();
+        new Thread(you).start();
+    }
+}
+
+class You extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<365*100;i++)
+            System.out.println("happy");
+        System.out.println("death");
+    }
+}
+class God extends Thread{
+    @Override
+    public void run() {
+        for(;;)
+            System.out.println("bless");
+    }
 }
